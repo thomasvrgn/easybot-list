81e70cf5559b3bd5fb1c3563ebd439bed198cea6
NessMC <contact@nessmc.fr>
2020-10-18 19:00:27 +0200

    💄 Header home UI added.

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,17 @@
 <template>
-  <navbar />
+  <main>
+    <navbar />
+    <header class="bg-indigo-100 py-8 px-8 mx-auto">
+      <div class="container mx-auto w-2/3">
+        <h1 class="text-2xl font-semibold text-gray-700">
+          Promoting your bots has never been easier
+        </h1>
+        <p class="text-gray-600 mt-4">
+          EasyList is a site and bot allowing you to promote your bot for free in order to attract more audience and increase your influence.
+        </p>
+      </div>
+    </header>
+  </main>
 </template>
 
 <script lang="ts">
