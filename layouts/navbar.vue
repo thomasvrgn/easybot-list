b2b6d417a4fbee252074883105837e9b5662bae3
NessMC <contact@nessmc.fr>
2020-10-19 14:50:16 +0200

    🐛 Responsive banner fixed.

--- layouts/navbar.vue
+++ layouts/navbar.vue
@@ -34,14 +34,14 @@
         </li>
       </ul>
     </div>
-    <div class="flex flex-auto py-4 flex-col lg:flex-row lg:w-6/12 lg:flex-initial" :class="{'hidden': !opened}">
-      <div class="container flex flex-row lg:flex-auto">
+    <div class="flex flex-auto py-4 flex-col lg:flex-row lg:flex-initial lg:space-x-8" :class="{'hidden': !opened}">
+      <div class="flex flex-row lg:flex-auto">
         <span class="bg-gray-900 items-center flex text-gray-800 px-2 rounded-l-full pt-1 cursor-pointer">
           <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M18.319 14.4326C20.7628 11.2941 20.542 6.75347 17.6569 3.86829C14.5327 0.744098 9.46734 0.744098 6.34315 3.86829C3.21895 6.99249 3.21895 12.0578 6.34315 15.182C9.22833 18.0672 13.769 18.2879 16.9075 15.8442C16.921 15.8595 16.9351 15.8745 16.9497 15.8891L21.1924 20.1317C21.5829 20.5223 22.2161 20.5223 22.6066 20.1317C22.9971 19.7412 22.9971 19.1081 22.6066 18.7175L18.364 14.4749C18.3493 14.4603 18.3343 14.4462 18.319 14.4326ZM16.2426 5.28251C18.5858 7.62565 18.5858 11.4246 16.2426 13.7678C13.8995 16.1109 10.1005 16.1109 7.75736 13.7678C5.41421 11.4246 5.41421 7.62565 7.75736 5.28251C10.1005 2.93936 13.8995 2.93936 16.2426 5.28251Z" fill="currentColor" /></svg>
         </span>
-        <input type="text" class="bg-gray-900 w-full py-2 rounded-r-full focus:outline-none placeholder-gray-600 text-gray-500" placeholder="Look for bots...">
+        <input type="text" class="bg-gray-900 w-full py-2 lg:w-64 rounded-r-full focus:outline-none placeholder-gray-600 text-gray-500" placeholder="Look for bots...">
       </div>
-      <div class="container flex flex-row pt-8 lg:pt-0 px-2 lg:px-0 items-center lg:flex-initial lg:justify-center">
+      <div class="flex flex-row pt-8 lg:pt-0 px-2 lg:px-0 items-center lg:flex-initial lg:justify-center">
         <div class="flex flex-row items-center lg:flex-initial">
           <img src="~/assets/user.png" class="w-12 rounded-full shadow-xl border border-gray-700" alt="">
           <span class="h-3 flex flex-row">
--- pages/index.vue
+++ pages/index.vue
@@ -2,14 +2,14 @@
   <main>
     <navbar />
     <header class="bg-gray-200 py-8 px-8 mx-auto">
-      <div class="container mx-auto w-2/3 lg:w-1/4">
+      <div class="container mx-auto w-2/3 lg:w-1/2">
         <h1 class="text-2xl font-semibold text-gray-700 lg:w-3/5">
           Promoting your bots has never been easier
         </h1>
         <p class="text-gray-600 mt-4">
           EasyList is a site and bot allowing you to promote your bot for free in order to attract more audience and increase your influence.
         </p>
-        <button class="bg-indigo-500 flex flex-row py-1 px-4 rounded mt-4 focus:outline-none">
+        <button class="bg-indigo-500 hover:bg-indigo-600 flex flex-row py-1 px-4 rounded mt-4 focus:outline-none">
           <span class="mr-2 text-white font-light">
             Start using 
           </span>
